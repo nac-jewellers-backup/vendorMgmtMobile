aa05cccab2b5936fc88488c1a10f8d007ec5c61c
nandhinirakshan123 <nandhini@infographicanalytics.com>
2023-08-10 12:13:46 +0530

    Navigation drawer

--- app/src/main/java/com/adminapp/nac_admin/Home/Home_screen.java
+++ app/src/main/java/com/adminapp/nac_admin/Home/Home_screen.java
@@ -1,9 +1,14 @@
 package com.adminapp.nac_admin.Home;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -14,18 +19,39 @@ import com.adminapp.nac_admin.enquiry.activity_enquiry;
 import com.adminapp.nac_admin.order.activity_order;
 import com.adminapp.nac_admin.payment.activity_payment;
 import com.adminapp.nac_admin.vendor.activity_vendor;
+import com.google.android.material.navigation.NavigationView;
 
 public class Home_screen extends AppCompatActivity {
 
     GridView grid_menu;
     Gridview_Adapter adapter;
 
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView Nav_drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        grid_menu=findViewById(R.id.gridview);
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.drawer_lay);
+        Nav_drawer=findViewById(R.id.nav_drawer);
+
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open,R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+       /* grid_menu=findViewById(R.id.gridview);
         adapter=new Gridview_Adapter(Home_screen.this);
 
         grid_menu.setAdapter(adapter);
@@ -62,7 +88,51 @@ public class Home_screen extends AppCompatActivity {
                         break;
                 }
             }
+        });*/
+
+        Nav_drawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.admin:
+
+                        Intent intent=new Intent(Home_screen.this,activity_admin.class);
+                        startActivity(intent);
+                        break;
+
+                  /*  case R.id.home_activity:
+                        return true;
+
+                    case R.id.task:
+                        startActivity(new Intent(getApplicationContext(), activity_task.class));
+                        overridePendingTransition(0, 0);
+                        finish();
+                        return true;
+
+                    case R.id.logout:
+                        MyAlertDialog alertDialog = new MyAlertDialog();
+                        alertDialog.ShowDialog(homescreen_activity.this,"Are you sure do you want to logout?");*/
+                }
+
+                return false;
+            }
         });
 
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
